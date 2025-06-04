b07f16ea761e82d0e487096d421013cf54dd4c9e
Brandon-Lea Price <brandonlea810@gmail.com>
2025-06-04 10:58:22 +0100

    Made the validation to each input and added style for the form validation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,18 +16,27 @@ function validateForm(event) {
     const email = form['email'].value;
     const message = form['message'].value;
 
-    if (name === '' || email === '' || message === '') {
-        alert('Please fill in all fields');
+    // Validate name
+    if(name === '') {
+        alert('Please enter a name');
+        return false;
+    }
+
+    // Validate email
+    if(email === '') {
+        alert('Please enter an email');
+        return false;
+    }
+
+    // Validate message
+    if(message === '') {
+        alert('Please enter a message');
         return false;
     }
 
     // Show confirmation dialog
     if (confirm('Are you sure you want to send this?')) {
-        // Here you would typically send the form data to a server
-        // For now, we'll just show a success message and reset the form
-        alert('Thank you for your message! We will get back to you soon.');
-
-
         form.submit();
+        alert('Thank you for your message! We will get back to you soon.');
     }
 }
