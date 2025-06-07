078178eca52549cee4de47eef358f6df9ee6fc25
Brandon-Lea Price <brandonlea810@gmail.com>
2025-06-07 21:35:53 +0100

    Added Dialog and removed formsubmit.io from index.html, style.css, script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,29 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+
+const confirmationDialog = document.getElementById('confirmation-dialog');
+const closeDialogButton = document.getElementById('close-dialog');
+
 document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
 
     hamburger.addEventListener('click', function() {
         navLinks.classList.toggle('active');
     });
 });
 
+closeDialogButton.addEventListener('click', function() {
+    confirmationDialog.close();
+});
+
+
 function validateForm(event) {
     // Prevent the default form submission
     event.preventDefault();
     
     const form = document.forms['contact-form'];
-    const name = form['subject'].value;
-    const email = form['email'].value;
-    const message = form['message'].value;
 
-    // Validate name
-    if(name === '') {
-        alert('Please enter a name');
-        return false;
-    }
 
-    // Validate email
-    if(email === '') {
-        alert('Please enter an email');
-        return false;
-    }
-
-    // Validate message
-    if(message === '') {
-        alert('Please enter a message');
-        return false;
-    }
-
-    // Show confirmation dialog
-    if (confirm('Are you sure you want to send this?')) {
-        form.submit();
-        alert('Thank you for your message! We will get back to you soon.');
-    }
+    confirmationDialog.showModal();
+    
 }
